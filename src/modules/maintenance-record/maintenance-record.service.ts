b778b59b2cdc37f37479a8693a4a6b01824ea0cc
Iyan Z Armanda <catcode0101@gmail.com>
2026-04-28 20:00:37 +0700

    chore: add logger on post services

--- src/modules/maintenance-record/maintenance-record.service.ts
+++ src/modules/maintenance-record/maintenance-record.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { PinoLogger } from 'nestjs-pino';
+import { BaseService } from '@/common/services/base.service';
 import { TruckService } from '@/modules/truck/truck.service';
 import { FacilityLocationService } from '@/modules/facility-location/facility-location.service';
 import { MaintenanceRecordRepository } from './maintenance-record.repository';
@@ -7,23 +9,59 @@ import type { MaintenanceRecord } from '@/generated/prisma/client';
 import type { CreateMaintenanceRecordDto } from './dtos/create-maintenance-record.dto';
 
 @Injectable()
-export class MaintenanceRecordService {
+export class MaintenanceRecordService extends BaseService {
   constructor(
     private readonly maintenanceRecordRepository: MaintenanceRecordRepository,
     private readonly truckService: TruckService,
     private readonly facilityLocationService: FacilityLocationService,
-  ) {}
+    readonly logger: PinoLogger,
+  ) {
+    super(logger);
+  }
 
   async create(body: CreateMaintenanceRecordDto): Promise<MaintenanceRecord> {
     const truck = await this.truckService.findUnique(body.truckId);
-    if (!truck) throw new NotFoundException('Truck ID does not exist');
-
+    if (!truck) {
+      this.logger.warn(
+        {
+          event: 'MAINTENANCE_RECORD_CREATE',
+          action: 'CHECK_TRUCK_ID',
+          truckIdTarget: body.truckId,
+          success: false,
+        },
+        'Missing Truck ID on create',
+      );
+      throw new NotFoundException('Truck ID does not exist');
+    }
     const facilityLocation = await this.facilityLocationService.findUnique(
       body.facilityLocation,
     );
-    if (!facilityLocation)
+    if (!facilityLocation) {
+      this.logger.warn(
+        {
+          event: 'MAINTENANCE_RECORD_CREATE',
+          action: 'CHECK_LOCATION',
+          facilityLocationTarget: body.facilityLocation,
+          success: false,
+        },
+        'Missing Facility Location on create',
+      );
       throw new NotFoundException('Facility Location does not exist');
+    }
 
-    return await this.maintenanceRecordRepository.create(body);
+    const maintenanceRecord =
+      await this.maintenanceRecordRepository.create(body);
+
+    this.logger.info(
+      {
+        event: 'MAINTENANCE_RECORD_CREATE',
+        action: 'CREATE_MAINTENANCE_RECORD',
+        maintenanceRecordIdTarget: maintenanceRecord.id,
+        success: true,
+      },
+      'Maintenance Record created',
+    );
+
+    return maintenanceRecord;
   }
 }
